9a1db928f1b62033c042498e793a3d5287d1248f
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-11 16:20:45 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: true,
+
   images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 31536000,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        port: "",
       },
     ],
   },
+
+  experimental: {
+    optimizeCss: true,
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
