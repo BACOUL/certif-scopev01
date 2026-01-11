1b472a8e81bff3c4b2174c949354e8b609d82ff4
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-11 12:52:25 +0100

    Create page.tsx

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="container py-20">
+      <h1 className="text-4xl font-bold">Pricing</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        Page in construction.
+      </p>
+    </div>
+  );
+}
