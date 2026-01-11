818b7f0558841b9cde663af5af7d97a9ecc5a556
TonPseudoGitHub <ton@email>
2026-01-11 17:55:56 +0100

    Add tailwindconfig + postcss + fix index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#0F172A',
-        accent: '#22C55E',
-        light: '#F8FAFC',
-        text: '#1E293B',
+        primary: "#1FB6C1",
+        dark: "#0B3A63",
       },
     },
   },
