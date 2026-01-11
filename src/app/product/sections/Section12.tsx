370dc5e1c920ec80b217525587f9059b84a7962b
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-11 22:15:49 +0100

    Create Section12.tsx

---
+++ src/app/product/sections/Section12.tsx
@@ -0,0 +1,9 @@
+export default function SectionX() {
+  return (
+    <section id="sX" className="scroll-mt-24">
+      <h2 className="text-3xl font-bold text-[#0B3A63] mb-6">Section X Title</h2>
+
+      {/* CONTENT HERE */}
+    </section>
+  );
+}
