d0705c10d5b8ff0ce0d05474d355eacaa01f0d20
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-14 22:04:11 +0100

    Delete src/app/product/compliance directory

--- src/app/product/compliance/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ComplianceLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="max-w-7xl mx-auto px-6 pt-8 pb-24">
-      {children}
-    </div>
-  );
-}
