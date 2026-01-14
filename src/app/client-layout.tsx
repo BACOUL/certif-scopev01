32245a32607c2473fc623d9d28088f3772a805e4
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-14 12:00:57 +0100

    Create client-layout.tsx

---
+++ src/app/client-layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import ScrollToTop from "@/components/ScrollToTop";
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+      <ScrollToTop />
+    </>
+  );
+}
