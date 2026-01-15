3fedc571e34f28d5ada732b6b75c15c7816c87f4
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-15 14:33:39 +0100

    Update client-layout.tsx

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -3,15 +3,25 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
+import { usePathname } from "next/navigation";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
   return (
     <>
       {/* HEADER FIXE */}
       <Header />
 
-      {/* OFFSET AUTOMATIQUE POUR TOUS LES ÉCRANS */}
-      <main className="pt-[90px] md:pt-[100px] lg:pt-[110px]">
+      {/* OFFSET ADAPTÉ */}
+      <main
+        className={
+          isHome
+            ? "pt-[40px] md:pt-[50px] lg:pt-[60px]"
+            : "pt-[90px] md:pt-[100px] lg:pt-[110px]"
+        }
+      >
         {children}
       </main>
 
