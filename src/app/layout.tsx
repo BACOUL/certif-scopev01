bf69f0e875d2656aada3aecf75173efbbf39e6fc
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-15 09:24:04 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,8 +83,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
 
       <body className="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200">
-        <ClientLayout>{children}</ClientLayout>
+        <ClientLayout>
+          <div className="pt-24">
+            {children}
+          </div>
+        </ClientLayout>
       </body>
     </html>
   );
-        }
+}
