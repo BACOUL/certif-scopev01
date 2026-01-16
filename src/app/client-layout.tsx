a1143b65c846ce08565053be197ddf727a9ace58
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-16 12:37:15 +0100

    Update client-layout.tsx

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,24 +1,28 @@
 "use client";
 
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
+export default function ClientLayout({ children }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const closeEvent = new Event("close-mobile-menu");
+    window.dispatchEvent(closeEvent);
+  }, [pathname]);
+
   return (
     <>
-      {/* HEADER FIXE */}
       <Header />
 
-      {/* OFFSET UNIFORME POUR TOUTES LES PAGES */}
       <main className="pt-[110px]">
         {children}
       </main>
 
-      {/* FOOTER */}
       <Footer />
-
-      {/* BOUTON SCROLL */}
       <ScrollToTop />
     </>
   );
