92852b9240df734ddd190ca2f400401d9e38a586
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-18 21:58:27 +0100

    Delete src/lib/logo.ts

--- src/lib/logo.ts
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export const LOGO_BASE64 = `data:image/png;base64,${fs
-  .readFileSync(path.join(process.cwd(), "public/logo1.png"))
-  .toString("base64")}`;
