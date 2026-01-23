1433705809e7e544dc232ed1b187316fb992b11b
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-01-23 14:28:16 +0100

    Update mailer.ts

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -6,22 +6,19 @@ export async function sendEmail({
   to,
   subject,
   html,
-  attachments,
 }: {
   to: string;
   subject: string;
   html: string;
-  attachments?: {
-    filename: string;
-    content: Buffer;
-    contentType: string;
-  }[];
 }) {
+  if (!process.env.RESEND_API_KEY) {
+    throw new Error("RESEND_API_KEY missing");
+  }
+
   return resend.emails.send({
     from: "Certif-Scope <no-reply@certif-scope.com>",
     to,
     subject,
     html,
-    attachments,
   });
 }
