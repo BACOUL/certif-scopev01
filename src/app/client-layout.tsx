32038a3cea2535f80c804bc3a5bfd78af262e671
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-02-01 23:25:38 +0100

    Update client-layout.tsx

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -2,12 +2,23 @@
 
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
+import HeaderFR from "@/components/fr/Header";
+import FooterFR from "@/components/fr/Footer";
+
 import ScrollToTop from "@/components/ScrollToTop";
 
-export default function ClientLayout({ children }) {
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const pathname = usePathname();
+
+  const isFR = pathname.startsWith("/fr");
 
   useEffect(() => {
     const closeEvent = new Event("close-mobile-menu");
@@ -16,13 +27,17 @@ export default function ClientLayout({ children }) {
 
   return (
     <>
-      <Header />
+      {/* HEADER */}
+      {isFR ? <HeaderFR /> : <Header />}
 
+      {/* CONTENT */}
       <main className="pt-[110px]">
         {children}
       </main>
 
-      <Footer />
+      {/* FOOTER */}
+      {isFR ? <FooterFR /> : <Footer />}
+
       <ScrollToTop />
     </>
   );
