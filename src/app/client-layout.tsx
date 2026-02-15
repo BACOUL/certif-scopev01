ebee4c31942fd4ad4a36993f3fb9afca333922fc
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-02-15 19:28:04 +0100

    Update client-layout.tsx

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -7,8 +7,13 @@ import { usePathname } from "next/navigation";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
 import HeaderFR from "@/components/fr/Header";
 import FooterFR from "@/components/fr/Footer";
+
+import HeaderDE from "@/components/de/Header";
+import FooterDE from "@/components/de/Footer";
+
 import ScrollToTop from "@/components/ScrollToTop";
 
 export default function ClientLayout({
@@ -23,21 +28,29 @@ export default function ClientLayout({
     return pathname === "/fr" || pathname.startsWith("/fr/");
   }, [pathname]);
 
+  const isDE = useMemo(() => {
+    if (!pathname) return false;
+    return pathname === "/de" || pathname.startsWith("/de/");
+  }, [pathname]);
+
   useEffect(() => {
     const closeEvent = new Event("close-mobile-menu");
     window.dispatchEvent(closeEvent);
   }, [pathname]);
 
+  const HeaderComponent = isDE ? HeaderDE : isFR ? HeaderFR : Header;
+  const FooterComponent = isDE ? FooterDE : isFR ? FooterFR : Footer;
+
   return (
     <>
-      {/* HEADER — EN/FR */}
-      {isFR ? <HeaderFR /> : <Header />}
+      {/* HEADER — EN/FR/DE */}
+      <HeaderComponent />
 
       {/* CONTENT */}
       <main className="pt-[110px]">{children}</main>
 
-      {/* FOOTER — EN/FR */}
-      {isFR ? <FooterFR /> : <Footer />}
+      {/* FOOTER — EN/FR/DE */}
+      <FooterComponent />
 
       <ScrollToTop />
     </>
